94c5d63ec023170e077b144f2028a4edc98855a3
Bekket McClane <yihshyng223@gmail.com>
2019-01-10 14:50:28 -0800

    Add more advanced lexer unittest testcases

--- src/Frontend/LexerTest.cpp
+++ src/Frontend/LexerTest.cpp
@@ -10,48 +10,68 @@ TEST(LexerTest, TestBasicTokens) {
     // Some keywords
     SS << "var function let if fi while do od";
     Lexer L(SS);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_VAR);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_FUNCTION);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_LET);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_IF);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_END_IF);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_WHILE);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_DO);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_END_DO);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_VAR);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_FUNCTION);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_LET);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IF);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_END_IF);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_WHILE);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_DO);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_END_DO);
   }
   SS.clear();
   {
     // numbers and identifiers
     SS << "87 yihshyng223 0 rem88the99best00";
     Lexer L(SS);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_NUMBER);
-    ASSERT_EQ(L.getBuffer(), "87");
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
-    ASSERT_EQ(L.getBuffer(), "yihshyng223");
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_NUMBER);
-    ASSERT_EQ(L.getBuffer(), "0");
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
-    ASSERT_EQ(L.getBuffer(), "rem88the99best00");
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_NUMBER);
+    EXPECT_EQ(L.getBuffer(), "87");
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
+    EXPECT_EQ(L.getBuffer(), "yihshyng223");
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_NUMBER);
+    EXPECT_EQ(L.getBuffer(), "0");
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
+    EXPECT_EQ(L.getBuffer(), "rem88the99best00");
   }
   SS.clear();
   {
     // assign and relation operators
     SS << "<- >= < <= != ==";
     Lexer L(SS);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_L_ARROW);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_L_ARROW);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_BIN_OP);
+  }
+}
+
+TEST(LexerTest, TestCompleteSentence) {
+  std::stringstream SS;
+  {
+    SS << "var foo;";
+    Lexer L(SS);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_VAR);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_SEMI_COLON);
   }
   SS.clear();
   {
-    // keywords, identifiers, and symbols
-    SS << "var foo;";
+    // with new line
+    SS << "var foo;" << "\n"
+       << "array[2] bar;";
     Lexer L(SS);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_VAR);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
-    ASSERT_EQ(L.getNextToken(), Lexer::TOK_SEMI_COLON);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_VAR);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_SEMI_COLON);
+
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_ARRAY);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_L_BRACKET);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_NUMBER);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_R_BRACKET);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_IDENT);
+    EXPECT_EQ(L.getNextToken(), Lexer::TOK_SEMI_COLON);
   }
+  // TODO: Test (multi-line)comments
 }
