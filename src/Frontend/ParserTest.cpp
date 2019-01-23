c1b97f1377573d959ef1c04c807144506cffaaf1
Bekket McClane <yihshyng223@gmail.com>
2019-01-23 14:35:56 -0800

    [Parser] Create AffineRecordTable
    For managing values in branches

--- src/Frontend/ParserTest.cpp
+++ src/Frontend/ParserTest.cpp
@@ -1,7 +1,71 @@
 #include "Parser.h"
-#include "gross/Graph/Graph.h"
-#include "gross/Graph/NodeUtils.h"
 #include "gtest/gtest.h"
-#include <sstream>
+#include <string>
 
 using namespace gross;
+
+TEST(ParserUtilsTest, TestAffineRecordTable) {
+  AffineRecordTable<std::string,int> ART;
+  using table_type = typename decltype(ART)::TableTy;
+  // should create a default scope and table
+  EXPECT_EQ(ART.num_scopes(), 1);
+  EXPECT_EQ(ART.num_tables(), 1);
+  ART["hello"] = 8;
+  ART["world"] = 7;
+  auto it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 8);
+  it_query = ART.find("world");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 7);
+
+  ART.NewAffineScope();
+  // should create a default table for the new scope
+  EXPECT_EQ(ART.num_scopes(), 2);
+  EXPECT_EQ(ART.num_tables(), 1);
+  // new scope should inherit old table at the beginning
+  it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 8);
+  it_query = ART.find("world");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 7);
+  // should copy a new table
+  ART["hello"] = 9;
+  ART["foo"] = 12;
+  it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 9);
+  it_query = ART.find("foo");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 12);
+  ART.CloseAffineScope<>(
+    [&](table_type* OutTable, const std::vector<table_type*>& Tables) {
+      EXPECT_EQ(Tables.size(), 1);
+      (*OutTable)["world"] = 100;
+    }
+  );
+  // should switch back to old table
+  EXPECT_EQ(ART.num_scopes(), 1);
+  it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 8);
+  it_query = ART.find("world");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 100);
+
+  ART.NewAffineScope();
+  ART["hello"] = 87;
+  ART.NewBranch();
+  // sibilin branches should not inteference each other
+  it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 8);
+
+  ART["hello"] = 94;
+  ART.NewAffineScope();
+  // should inhereit from the second branch
+  it_query = ART.find("hello");
+  ASSERT_NE(it_query, ART.end());
+  EXPECT_EQ(it_query->second, 94);
+}
