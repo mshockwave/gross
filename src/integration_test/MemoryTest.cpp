62241344870f754c67768036dd8cc9cf1c956837
Bekket McClane <yihshyng223@gmail.com>
2019-03-06 09:41:28 -0800

    [Graph] Add more integration test for memory operations
    Regarding loop structures.

--- src/integration_test/MemoryTest.cpp
+++ src/integration_test/MemoryTest.cpp
@@ -40,3 +40,35 @@ TEST(MemoryIntegrateTest, TestBasicControlStructure) {
   }
 }
 
+TEST(MemoryIntegrateTest, TestSimpleLoop) {
+  std::ifstream IF("memory2.txt");
+
+  Graph G;
+  Parser P(IF, G);
+  (void) P.getLexer().getNextToken();
+  P.NewSymScope(); // global scope
+
+  ASSERT_TRUE(P.ParseFuncDecl());
+  {
+    std::ofstream OF("TestMemorySimpleLoop.dot");
+    G.dumpGraphviz(OF);
+  }
+
+  GraphReducer::RunWithEditor<ValuePromotion>(G);
+  {
+    std::ofstream OF("TestMemorySimpleLoop.mem2reg.dot");
+    G.dumpGraphviz(OF);
+  }
+
+  GraphReducer::RunWithEditor<MemoryLegalize>(G);
+  {
+    std::ofstream OF("TestMemorySimpleLoop.mem2reg.legal.dot");
+    G.dumpGraphviz(OF);
+  }
+
+  GraphReducer::RunWithEditor<PeepholeReducer>(G);
+  {
+    std::ofstream OF("TestMemorySimpleLoop.mem2reg.legal.peephole.dot");
+    G.dumpGraphviz(OF);
+  }
+}
